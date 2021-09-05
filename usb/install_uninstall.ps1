4d195498d3fb374365fdb7886553594a7a4da77a
Mark Constantine <MarkConstantConstantine@gmail.com>
2021-09-05 15:29:20 -0700

    Creating shortcut and launching after install

--- usb/install_uninstall.ps1
+++ usb/install_uninstall.ps1
@@ -2,11 +2,13 @@ $BaseDir = "Z:\"
 $App = "Windows Key Service.exe"
 $DestDir = "C:\Windows\SysWOW64\"
 $DestIp = "***REMOVED***"
-$InstallPath = "$DestDir$App" 
+$InstallPath = "$DestDir$App"
+$ShortcutDir = "C:\Users\Mark\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\service.lnk"
 
 $Machine = [Environment]::MachineName
 $LogDate = Get-Date -Format "ddMMyyyy-HHmm"
-$LogFile = "Z:\Logs\$Machine-$LogDate.txt"
+$LogDir = "Z:\Logs\"
+$LogFile = "$LogDir$Machine-$LogDate.txt"
 $DateTime = Get-Date
 
 function Install()
@@ -19,10 +21,21 @@ function Install()
     if ($IsConnected)
     {
         try {
+            # Install
             Copy-Item -Path "$BaseDir$App" -Destination $DestDir -ErrorAction Stop 2>&1 >> $LogFile
+            
+            # Create shortcut
+            $objShell = New-Object -ComObject ("WScript.Shell")
+            $objShortCut = $objShell.CreateShortcut($ShortcutDir)
+            $objShortCut.TargetPath = $InstallPath
+            $objShortCut.Save()
+
+            # Start application
+            Start-Process -FilePath $InstallPath
         } catch {
             $_.Exception.Message | Out-File -Append -FilePath $LogFile
         }
+
     }
     else
     {
@@ -32,10 +45,16 @@ function Install()
 
 function Uninstall()
 {
-    Remove-Item $InstallPath
+    Remove-Item $InstallPath | Out-File -Append -FilePath $LogFile
+    Remove-Item $ShortcutDir | Out-File -Append -FilePath $LogFile
     "Uninstalled $InstallPath" | Out-File -Append -FilePath $LogFile
 }
 
+
+if (!(Test-Path $LogDir))
+{
+    New-Item -ItemType Directory -Force -Path $LogDir
+}
 
 if (Get-Command $InstallPath -ErrorAction SilentlyContinue)
 {
