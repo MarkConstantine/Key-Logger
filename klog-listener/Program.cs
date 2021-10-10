6f44b224c4795dd6e65b99c48f624ff0fa9a232a
Mark Constantine <MarkConstantConstantine@gmail.com>
2021-10-09 23:02:35 -0700

    klog-listener as Windows service

--- klog-listener/Program.cs
+++ klog-listener/Program.cs
@@ -1,11 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace klog_listener
 {
@@ -18,6 +12,7 @@ namespace klog_listener
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
